ac905141fb2cca5c3755b543c754e59676822c65
Cody Reece <codyreece323@gmail.com>
2022-11-06 14:12:26 -0600

    Add files via upload

---
+++ app/scripts/components/reaction-picker.js
@@ -0,0 +1,36 @@
+import m from 'mithril';
+
+class ReactionPickerComponent {
+
+  oninit({ attrs: { game, session } }) {
+    this.game = game;
+    this.session = session;
+  }
+
+  sendReaction(reaction) {
+    this.session.emit('send-reaction', { reaction });
+  }
+
+  view() {
+    return m('div#reaction-picker', ReactionPickerComponent.availableReactions.map((reaction) => {
+      return m('div.available-reaction', m('div.available-reaction-symbol', {
+        onclick: () => this.sendReaction(reaction)
+      }, reaction.symbol));
+    }));
+  }
+
+}
+
+ReactionPickerComponent.availableReactions = [
+  { symbol: '🖕' },
+  { symbol: '🥵' },
+  { symbol: '✊' },
+  { symbol: '🍻' },
+  { symbol: '🍆🤤' },
+  { symbol: '👉👌' },
+  { symbol: 'Rematch?' },
+  { symbol: 'Yes  ' },
+  { symbol: '  No' },
+];
+
+export default ReactionPickerComponent;
